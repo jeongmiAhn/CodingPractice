228f9640afb37e71f4c25dbd86c9b0a0b69039d7
jeongmi Ahn <me_meome@naver.com>
2021-08-16 16:35:10 +0900

    백준 문제 풀이

---
+++ src/baekjoon/level1/Hello_World/Main.java
@@ -0,0 +1,9 @@
+package baekjoon.level1.Hello_World;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!");
+	}
+}
---
+++ src/baekjoon/level1/We_love_kriii/Main.java
@@ -0,0 +1,9 @@
+package baekjoon.level1.We_love_kriii;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		System.out.println("강한친구 대한육군\n강한친구 대한육군");
+	}
+}
