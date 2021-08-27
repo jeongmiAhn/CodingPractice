db1b0906f48eda1531918dbd6a7c0da7d3c3fba4
jeongmi Ahn <me_meome@naver.com>
2021-08-27 17:46:55 +0900

    백준 문제 풀이

---
+++ src/baekjoon/level1/A____B/Main.java
@@ -0,0 +1,18 @@
+package baekjoon.level1.A____B;
+
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		double a = sc.nextInt();
+		double b = sc.nextInt();
+		
+		System.out.println(a/b);
+		
+		sc.close();
+	}
+}
