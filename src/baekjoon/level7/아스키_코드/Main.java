816fd1cd105cfa07b418e4db7c720c87833450a5
jeongmi Ahn <me_meome@naver.com>
2021-09-03 15:16:50 +0900

    백준 문제 풀이

---
+++ src/baekjoon/level7/아스키_코드/Main.java
@@ -0,0 +1,15 @@
+package baekjoon.level7.아스키_코드;
+
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		int asc = sc.next().charAt(0);
+		
+		System.out.println(asc);
+	}
+}
