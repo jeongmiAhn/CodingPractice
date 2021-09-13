8242f3de06455ccb3ada226af67e4565024684a4
jeongmi Ahn <me_meome@naver.com>
2021-09-13 17:39:08 +0900

    백준 문제 풀이

---
+++ src/baekjoon/level7/단어의_개수/Main.java
@@ -0,0 +1,19 @@
+package baekjoon.level7.단어의_개수;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+ 
+public class Main
+{ 
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+ 
+		String S = sc.nextLine();
+		sc.close();
+ 
+		StringTokenizer st = new StringTokenizer(S," ");
+		
+		System.out.println(st.countTokens());	
+	}
+}
